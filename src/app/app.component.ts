3f7640076c3cf37e279ffb32d4e688499fea4067
Rupa09 <rupaanupati09@gmail.com>
2022-07-08 10:34:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'counterapp';
+  //countNumber
+  countNumber = 0;
+
+  //reset
+  resetBtn() {
+    this.countNumber = 0;
+  }
+  //increaseBtn
+  increaseBtn() {
+    this.countNumber = this.countNumber + 1;
+  }
+  //decreaseBtn
+  decreaseBtn() {
+    this.countNumber = this.countNumber - 1;
+    if (this.countNumber > 0) {
+      this.countNumber = 0;
+    }
+  }
 }
